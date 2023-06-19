184f037db1acb78839cf5d020ea3efc9a8a5f3ed
majimearun <arunachala0619@gmail.com>
2023-06-19 17:04:50 +0530

    chore: rename files

---
+++ insight_generation.py
@@ -0,0 +1,13 @@
+# imports
+
+import pandas as pd
+
+hospitals = pd.read_csv("./data/hospitals.csv")
+patients = pd.read_csv("./data/patients.csv")
+visits = pd.read_csv("./data/visits.csv")
+doctors = pd.read_csv("./data/doctors.csv")
+diseases = pd.read_csv("./data/diseases.csv")
+medicines = pd.read_csv("./data/medicines.csv")
+observations = pd.read_csv("./data/observations.csv")
+prescriptions = pd.read_csv("./data/prescriptions.csv")
+tests = pd.read_csv("./data/tests.csv")
