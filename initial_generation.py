fdbd95e2cae1b654f68dd4e4d1bba5169e431824
majimearun <arunachala0619@gmail.com>
2023-06-19 16:47:22 +0530

    feat: imp fill visits for more coherent data wrt location/hospital visited

--- initial_generation.py
+++ initial_generation.py
@@ -171,12 +171,38 @@ def fill_doctors(n):
 def fill_visits(n):
     for i in range(n):
         patient_id = fake.random_int(min=0, max=patients.shape[0] - 1)
+
+        hospitals_in_country = hospitals[
+            hospitals["country"] == patients.loc[patient_id]["country"]
+        ]["hospital_id"].unique()
+
         if visits[visits["patient_id"] == patient_id].shape[0] > 0:
             max_date = visits[visits["patient_id"] == patient_id]["date_of_visit"].max()
+
+            hospitals_visited = visits[visits["patient_id"] == patient_id][
+                "hospital_id"
+            ].unique()
+
+            hospitals_not_visited = list(
+                set(hospitals_in_country) - set(hospitals_visited)
+            )
+            if len(hospitals_not_visited) > 0:
+                new_hospital = fake.random_element(elements=hospitals_not_visited)
+            repeat_hospital = fake.random_element(elements=hospitals_visited)
+            if len(hospitals_not_visited) > 0:
+                prob = random.random()
+                if prob < 0.2:
+                    hospital_id = new_hospital
+                else:
+                    hospital_id = repeat_hospital
+            else:
+                hospital_id = repeat_hospital
+        else:
+            hospital_id = fake.random_element(elements=hospitals_in_country)
         visits.loc[i] = [
             i,
             patient_id,
-            fake.random_int(min=0, max=hospitals.shape[0] - 1),
+            hospital_id,
             None,
             fake.random_int(min=100, max=10000),
         ]
